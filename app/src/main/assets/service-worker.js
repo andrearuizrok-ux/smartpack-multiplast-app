7cd3b8679a187b159dba1ca633ab26ddfafe7847
andrearuizrok-ux <andrearuizrok@gmail.com>
2026-09-19 18:39:34 -0400

    Add files via upload

--- app/src/main/assets/service-worker.js
+++ app/src/main/assets/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='spmp-v116-static-18';
+const CACHE='spmp-v116-static-19';
 const STATIC=['./','./index.html','./smartpack-v11.js','./access-v11.5.js','./planner-v11.5.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./IndustrialOS_Modello_Import_Dati_Azienda.xlsx'];
 
 self.addEventListener('install',event=>{
